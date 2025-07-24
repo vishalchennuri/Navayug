5918d9477c38601ff352d6e86143c0280d3ac5b8
Vishal chennuri <vishalnetha7@gmail.com>
2025-07-24 21:21:41 +0530

    x

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// hello gois
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
