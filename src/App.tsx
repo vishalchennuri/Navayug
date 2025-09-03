62466488e71f624e285a85cee1ae1095bb3bf9b2
Vishal chennuri <vishalnetha7@gmail.com>
2025-09-03 13:04:41 +0530

    bug fix

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import Services from "./pages/Services";
 import PlansAndPricing from "./pages/Plans";
 import Contact from "./pages/ContactUS";
 import ErrorPage from "./pages/Error";
-import Portfolio from "./components/PortfolioWebsite";
 import WorkPortfolio from "./components/WorkDetails";
 import PortfolioPage from "./pages/Portfolio";
 
